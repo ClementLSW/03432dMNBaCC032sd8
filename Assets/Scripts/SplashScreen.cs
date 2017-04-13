906f214cd1c1e1ed22daf0df17f575d3e82dabf5
Hyde <liquid_assassin@hotmail.com>
2017-04-13 19:30:28 +0800

    Added splash screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour {
+
+	public MovieTexture splash;
+	public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = splash;
+		splash.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!splash.isPlaying) 
+		{
+			canvas.SetActive (true);
+			Destroy (gameObject);
+		}
+			
+	}
+}
